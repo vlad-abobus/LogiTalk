0710cfe8b24cf3ff96d74d77cb0c7ff83882c43c
VladislavMorgan <144521902+vlad-abobus@users.noreply.github.com>
2025-09-27 12:18:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,27 @@ HOST = '0.0.0.0'
 PORT = 8080
 
 clients = []
+    def broadcast(data , exclude_socket=None):
+        for client in clients:
+            if client != exclude_socket:
+                try:
+                    client.sendall(data)
+                except:
+                    pass
+    def handle_client(client_socket):
+        while True:
+            try:
+                data = client_socket.recv(4096)
+                if not data:
+                    break
+                broadcast(data , exclude_socket=client_socket)
+            except:
+                break
+        if client_socket in clients:
+            clients.remove(client_socket)
+        client_socket.close()
+
+
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,7 +37,7 @@ def main():
         client_socket , add = (server_socket.accept())
         print(f"Клієнт підключився  ")
         clients.append(client_socket)
-        t = threading.Thread()
+        t = threading.Thread(target=handle_client , args=(client_socket,))
         t.start()
 if __name__ == "main":
     main()
